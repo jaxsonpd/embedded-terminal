940c2f80323585432799744a7b5e1e4930d6860b
Jack Duignan <jackpduignan@gmail.com>
2024-04-06 18:45:54 +1300

    Added to the help description

--- src/help.c
+++ src/help.c
@@ -13,10 +13,18 @@
 
 
 uint8_t HELP_entry(char* args) {
-    UART_puts("Embedded Terminal version 0.1\n");
+    UART_puts("Embedded Terminal version 0.1\n\n");
+
+    UART_puts("Use info embeddedTerminal for more infomation about this shell\n");
+    UART_puts("Commands marked with * are not implemented in this system\n");
     UART_puts("The following commands are available for the system:\n\n");
+    
     UART_puts("help: print this message\n");
     UART_puts("clear: clear the screen\n");
+    UART_puts("*info: display information about the command\n");
+    UART_puts("*neofecth: displays information about this system\n");
+    UART_puts("*led: toggels the led connected to PB5\n");
+    UART_puts("*digitalWrite: writes to a output of the microcontroller\n");
 
     return 0;
 }
