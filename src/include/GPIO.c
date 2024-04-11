f0baf7f1c4e6a7401a5d64588ad90011660ae920
Jack Duignan <jackpduignan@gmail.com>
2024-04-11 21:08:06 +1200

    Started GPIO module

--- src/include/GPIO.c
+++ src/include/GPIO.c
@@ -2,12 +2,46 @@
  * @file GPIO.c
  * @author Jack Duignan (JackpDuignan@gmail.com)
  * @date 2024-04-07
- * @brief Module for handelling the IO ports
+ * @brief A library to control the GPIO on the microcontroller
  */
 
 
 #include <stdint.h>
 #include <stdbool.h>
+
 #include <avr/io.h>
 
-DDRB
+#include "GPIO.h"
+
+void GPIO_pinInit(uint16_t pin, pinType_t type) {
+    // Set the DDR and PORT to match the type
+    volatile uint8_t *DDR_pin = PINBANK(pin) - 1; // DDRX is one regs lower then PORTX 
+    
+    if (type == OUTPUT) {
+        *DDR_pin |= (1 << PINNUM(pin));
+    } else if (type == INPUT_PULLUP) {
+        *DDR_pin &= ~(1 << PINNUM(pin));
+        *(uint8_t *)PINBANK(pin) &= (1 << PINNUM(pin)); // set pullup
+    } else if (type == INPUT_NO_PULLUP) {
+        *DDR_pin &= ~(1 << PINNUM(pin));
+        *(uint8_t *)PINBANK(pin) &= ~(1 << PINNUM(pin)); // set pullup
+    }
+}
+
+
+void GPIO_setOutput(uint16_t pin, bool value) {
+    if (value) {
+        *(uint8_t *)PINBANK(pin) &= (1 << PINNUM(pin));
+    } else {
+        *(uint8_t *)PINBANK(pin) &= ~(1 << PINNUM(pin));
+    }
+}
+
+
+bool GPIO_toggleOutput(uint16_t pin) {
+    return 0;
+}
+
+bool GPIO_getState(uint16_t pin) {
+    return 0;
+}
---
+++ src/include/GPIO.h
@@ -0,0 +1,77 @@
+/** 
+ * @file GPIO.h
+ * @author Jack Duignan (JackpDuignan@gmail.com)
+ * @date 2024-04-11
+ * @brief A library to control the GPIO on the microcontroller
+ */
+
+
+#ifndef GPIO_H
+#define GPIO_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/// Define a pin for use in the GPIO functions should be asigned to a uint16_t
+#define PIN(bank, num) (((bank - 'A') << 8) | (num))
+
+/// Extract the pins number
+#define PINNUM(pin) (pin & 255) 
+
+/// Extract the pins bank
+#define PINBANK(pin) (pin >> 8) 
+
+
+/// The different types of GPIO pins
+typedef enum {
+    OUTPUT,
+    INPUT_NO_PULLUP,
+    INPUT_PULLUP,
+} pinType_t;
+
+
+/** 
+ * @brief Initalise the pin
+ * @param pin the pin defined using the PIN macro
+ * @param type the pin type (OUTPUT, INPUT_NO_PULLUP, INPUT_PULLUP)
+ * 
+ */
+void GPIO_pinInit(uint16_t pin, pinType_t type);
+
+
+/** 
+ * @brief Set a pins output state
+ * @param pin the pin to set the output state for
+ * @param value the output value
+ */
+void GPIO_setOutput(uint16_t pin, bool value);
+
+
+/** 
+ * @brief toggle the output of a pin
+ * @param pin the pin to toggel the state of
+ * 
+ * @return the state of the pin
+ */
+bool GPIO_toggleOutput(uint16_t pin);
+
+
+/** 
+ * @brief Get the current state of the pin
+ * @param pin the pin to read the state of
+ * 
+ * @return the state of the pin
+ */
+bool GPIO_getState(uint16_t pin);
+
+
+
+
+
+
+
+
+
+
+#endif // GPIO_H
