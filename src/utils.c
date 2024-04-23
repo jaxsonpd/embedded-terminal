dc1d2f534984c0232bc3d868687dc6382123db46
Jack Duignan <jackpduignan@gmail.com>
2024-04-23 12:40:20 +1200

    Getopt implemented but not tested

--- src/utils.c
+++ src/utils.c
@@ -65,3 +65,89 @@ uint8_t UTL_getLineWithEcho (char *prmpt, char *buffer, size_t buffSize) {
     return 0;
 }
 
+/// @brief The options argument string 
+char *optarg; 
+
+/// @brief The index of the next element to be processed in argv
+int16_t optind=1;
+
+/// @brief If this is set to zero then no error messages are printed by getopt()
+int16_t opterr=1;
+
+/// @brief Where an error option is placed
+char optopt;
+
+
+/** 
+ * @brief Get the options from a list of command line options.
+ *  
+ * This implementation is based on the GNU implementation of getopt() as often defined in <unistd.h>
+ * 
+ * @cite https://man7.org/linux/man-pages/man3/getopt.3.html
+ * @related optarg, optind, opterr, optopt
+ * 
+ * @param argc the number of arguments passed
+ * @param argv the list of each argument passed
+ * @param optstring the string that defines the available options
+ * 
+ * @return successively each option character and -1 at end, '?' is returned when an option is not recognised and ':' 
+ * returned when a required argument is not provided
+ */
+int16_t getopt (int argc, char* argv[], char *optstring) {
+    bool isopt = false;
+    bool reqarg = false;
+    bool reqargopt = false;
+
+    char* opt = argv[optind];
+
+    // Check to see if opt should be processed
+    if (optind >= argc) return -1;
+
+    if (opt[0] == '-') { // it is an option
+        for (uint16_t i; i >= strlen(optstring); i++) {
+            if (optstring[i] == opt[1]) {
+                if (optstring[i+1] == ':') {
+                    if (optstring[i+2] == ':') reqargopt = true;
+                    else reqarg = true;
+                }
+                isopt = true;
+                break;
+            }
+        }
+    } else { // Technically should permute through here but for now ignore and move on
+        optind++;
+        return getopt(argc, argv, optstring);
+    }
+
+    // Process opt
+    if (!isopt) {
+        optopt = argv[optind][1];
+        optind++;
+        // need to include printing error message
+        return '?';
+    } else if (reqarg) {
+        if ((optind != argc) && (argv[optind+1][0] != '-')) { // There is an argument
+            optarg = argv[optind+1];
+            optind += 2;
+            return opt[1];
+        } else {
+            optind++;
+            optopt = opt;
+            return ':';
+        }
+    } else if (reqargopt) {
+        if ((optind != argc) && (argv[optind+1][0] != '-')) { // There is an argument
+            optarg = argv[optind+1];
+            optind += 2;
+        } else {
+            optind++;
+        }
+
+        return opt[1];
+    } else {
+        optind++;
+        return opt[1];
+    }
+}
+
+
