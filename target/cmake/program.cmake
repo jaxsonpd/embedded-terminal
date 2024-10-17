4ba7bbf3e08b768b8f78f7418cf044ea00b56859
Jack Duignan <jackpduignan@gmail.com>
2024-10-17 20:32:54 +1300

    CMAKE working

--- target/cmake/program.cmake
+++ target/cmake/program.cmake
@@ -1,6 +1,8 @@
 find_program(AVRDUDE avrdude REQUIRED)
 message(STATUS "Found avrdude: ${AVRDUDE}")
 
+set(USBDEVICE /dev/ttyUSB0)
+
 function(add_program_target EXECUTABLE_TARGET)
     cmake_parse_arguments(
         PARSE_ARGV 1 ARGS "" "PROGRAM_TARGET;DEBUG_TARGET" ""
@@ -9,14 +11,12 @@ function(add_program_target EXECUTABLE_TARGET)
         message(SEND_ERROR "No target name '${EXECUTABLE_TARGET}'")
     endif()
 
+    set(EXE_PATH $<TARGET_FILE:${EXECUTABLE_TARGET}>)
     set(program_target program-${EXECUTABLE_TARGET})
 
     add_custom_target(
         ${program_target}
-        DEPENDS 
-        COMMAND 
-            ${AVRDUDE} -F -V -c arduino -p ATMEGA328P -P ${USBDEVICE} -b 115200 -U flash:w:$(BUILDDIR)/$(TARGET).hex
+        DEPENDS ${EXE_PATH}
+        COMMAND ${AVRDUDE} -F -V -c arduino -p ATMEGA328P -P ${USBDEVICE} -b 115200 -U flash:w:${EXE_PATH}
     )
-
-
 endfunction()
